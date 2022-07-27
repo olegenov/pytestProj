c8b242d856c07ff4c588405ddc53bad866d07ff4
olegenov <nik_kitaev@mail.ru>
2022-07-27 11:31:49 +0300

    locators added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,6 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
 from .locators import MainPageLocators
+
 
 class MainPage(BasePage):
     def go_to_login_page(self):
