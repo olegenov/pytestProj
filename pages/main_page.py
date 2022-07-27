8faaad670eddd0ec8c57b9fa6875a3cf8c8b03a1
olegenov <nik_kitaev@mail.ru>
2022-07-27 11:48:05 +0300

    Login PageObj added

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,4 +9,4 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), \
-               "Login link is not presented"
+               'Login link is not presented'
