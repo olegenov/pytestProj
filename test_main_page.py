318e1ddb55a85ee219c269abdc5c2ee372417ce8
olegenov <nik_kitaev@mail.ru>
2022-07-27 11:52:15 +0300

    test_main_page updated

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,15 @@
 from selenium.webdriver.common.by import By
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
+
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
